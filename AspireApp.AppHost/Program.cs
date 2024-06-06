320967a6b7d9bc7b1077d7a1f1db36eb382afef9
Ratko Ćosić <ratko_cosic@hotmail.com>
2024-06-06 06:25:21 +0200

    GA update

--- AspireApp.AppHost/Program.cs
+++ AspireApp.AppHost/Program.cs
@@ -5,6 +5,7 @@ var cache = builder.AddRedis("cache");
 var apiService = builder.AddProject<Projects.AspireApp_ApiService>("apiservice");
 
 builder.AddProject<Projects.AspireApp_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
     .WithReference(cache)
     .WithReference(apiService);
 
